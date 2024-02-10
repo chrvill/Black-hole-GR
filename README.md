09cac893f0a707c25f61a3e962106fe0d2d09a1b
= <cvillandseie@gmail.com>
2024-02-10 20:19:04 +0100

    Fixed color of math expression and updated metric.py

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ Takes a metric and uses the Sympy Python package to derive the equations of moti
 
 These equations of motion are the components of the geodesic equation
 
-<img src="https://latex.codecogs.com/svg.image?\large%20\frac{d^2x^\mu}{d\lambda^2}%20=%20-\Gamma^\mu_{\rho%20\sigma}%20\frac{dx^\rho}{d\lambda}%20\frac{dx^\sigma}{d\lambda}" />
+<img src="https://latex.codecogs.com/svg.image?\large%20\color{white}\frac{d^2x^\mu}{d\lambda^2}%20=%20-\Gamma^\mu_{\rho%20\sigma}%20\frac{dx^\rho}{d\lambda}%20\frac{dx^\sigma}{d\lambda}" />
 
 The right hand side referred to in the code is thus the right hand side of the above equation, so the second derivative of <img src="https://latex.codecogs.com/svg.image?\large%20x^\mu" /> with respect to <img src="https://latex.codecogs.com/svg.image?\large%20\lambda" />. The metric given as input to the instances of the Metric class is <img src="https://latex.codecogs.com/svg.image?\large%20g_{\mu%20\nu}" />.
 
--- metric.py
+++ metric.py
@@ -8,7 +8,7 @@ class Metric:
     Input arguments:
         g:
             sympy.Matrix
-            Contains the covariant components of the metric tensor
+            Contains the covariant components of the metric tensor (g_(mu nu))
 
         coordinates:
             list/array of sympy.Symbol
@@ -25,7 +25,7 @@ class Metric:
         self.g_inv = self.g.inv()       # The contravariant ---------||----------------
         self.coordinates = coordinates  # The coordinates in which we express the metric
 
-        self.dim = len(self.coordinates)  # Dimension of the spacetime in question (will assume 4-dim in other parts of the code. That should be generalized)
+        self.dim = len(self.coordinates)  # Dimension of the spacetime in question (have assumed 4-dim in other parts of the code. That should be generalized)
 
         # Sympy variables representing the components of the four-velocity of a particle
         # u0 is dx^0/dlambda, u1 is dx^1/dlambda etc.
@@ -71,25 +71,37 @@ def getMetric(name):
     and returns a Metric-object containing the corresponding metric. Also returns
     a list containing all the variables entering the metric, as sympy.symbols.
     """
-    t, r, theta, phi, M, a = sympy.symbols("t, r, theta, phi, M, a")
+    t, r, theta, phi, M, a, v = sympy.symbols("t, r, theta, phi, M, a, v")
 
     coordinates = [t, r, theta, phi]
 
     sigma = r**2 + a**2*sympy.cos(theta)**2
-    delta = r**2 - 2*M*r + a**2
+    delta = r**2 - 2*r + a**2
 
     # Kerr metric
-    g = sympy.Matrix([[-(1 - 2*M*r/sigma), 0, 0, -2*a*M*r/sigma*sympy.sin(theta)**2],
+    g = sympy.Matrix([[-(1 - 2*r/sigma), 0, 0, -2*a*r/sigma*sympy.sin(theta)**2],
                       [0, sigma/delta, 0, 0], [0, 0, sigma, 0],
-                      [-2*a*M*r/sigma*sympy.sin(theta)**2, 0, 0,
-                      (r**2 + a**2 + 2*M*r*a**2/sigma*sympy.sin(theta)**2)*sympy.sin(theta)**2]])
+                      [-2*a*r/sigma*sympy.sin(theta)**2, 0, 0,
+                      (r**2 + a**2 + 2*r*a**2/sigma*sympy.sin(theta)**2)*sympy.sin(theta)**2]])
 
     kerr = Metric(g, coordinates)
 
     # Schwarzschild metric
-    g = sympy.Matrix([[-(1 - 2*M/r), 0, 0, 0], [0, (1 - 2*M/r)**(-1), 0, 0], [0, 0, r**2, 0], [0, 0, 0, r**2*sympy.sin(theta)**2]])
+    g = sympy.Matrix([[-(1 - 2*M/r), 0, 0, 0],
+                      [0, (1 - 2*M/r)**(-1), 0, 0],
+                      [0, 0, r**2, 0],
+                      [0, 0, 0, r**2*sympy.sin(theta)**2]])
 
     schwarzschild = Metric(g, coordinates)
 
-    metrics = {"kerr": [kerr, [t, r, theta, phi, M, a]], "schwarzschild": [schwarzschild, [t, r, theta, phi, M]]}
+    g = sympy.Matrix([[-(1 - 2/r), 1, 0, 0],
+                      [1, 0, 0, 0],
+                      [0, 0, r**2, 0],
+                      [0, 0, 0, r**2*sympy.sin(theta)**2]])
+
+    coordinates = [v, r, theta, phi]
+    schwarzschild_edfink = Metric(g, coordinates)
+
+    metrics = {"kerr": [kerr, [t, r, theta, phi, M, a]], "schwarzschild": [schwarzschild, [t, r, theta, phi, M]], \
+               "schwarzschild_edfink": [schwarzschild_edfink, [v, r, theta, phi]]}
     return metrics[name]
